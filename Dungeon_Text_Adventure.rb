81dbbd0a7f63a52e7a4ae652a68c38fb02f42d7a
oszegda <oszegda@icloud.com>
2022-12-13 23:24:15 +0100

    Creating the Initial Classes

--- Dungeon_Text_Adventure.rb
+++ Dungeon_Text_Adventure.rb
@@ -1,2 +1,33 @@
 # frozen_string_literal: true
 
+# top-level documentation
+class Dungeon
+  attr_accessor :player
+
+  def initialize(player)
+    @player = player
+    @rooms = {}
+  end
+end
+
+# top-level documentation
+class Player
+  attr_accessor :name, :location
+
+  def initialize(player_name)
+    @name = player_name
+  end
+end
+
+# top-level documentation
+class Room
+  attr_accessor :reference, :name, :description, :connections
+
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+end
+
