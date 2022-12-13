100548fe3d333f3d45959af65639a7505f52faed
oszegda <oszegda@icloud.com>
2022-12-13 23:33:59 +0100

    Creating Rooms

--- Dungeon_Text_Adventure.rb
+++ Dungeon_Text_Adventure.rb
@@ -8,8 +8,16 @@ class Dungeon
     @player = player
     @rooms = {}
   end
+
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
 end
 
 Player = Struct.new(:name, :location)
 Room = Struct.new(:reference, :name, :description, :connections)
 
+me = Player.new("Fred Bloggs")
+my_dungeon = Dungeon.new(me)
+my_dungeon.add_room(:largecave, 'Large Cave', 'a large cavernous cave', { west: :smallcave })
+my_dungeon.add_room(:smallcave, 'Small Cave', 'a small, claustrophobic cave', { east: :largecave })
