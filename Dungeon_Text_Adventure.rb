8d57c1f68f3ea307bd6b76970faee49e7faf9538
oszegda <oszegda@icloud.com>
2022-12-13 23:26:37 +0100

    Structs: Quick and Easy Data Classes

--- Dungeon_Text_Adventure.rb
+++ Dungeon_Text_Adventure.rb
@@ -10,24 +10,6 @@ class Dungeon
   end
 end
 
-# top-level documentation
-class Player
-  attr_accessor :name, :location
-
-  def initialize(player_name)
-    @name = player_name
-  end
-end
-
-# top-level documentation
-class Room
-  attr_accessor :reference, :name, :description, :connections
-
-  def initialize(reference, name, description, connections)
-    @reference = reference
-    @name = name
-    @description = description
-    @connections = connections
-  end
-end
+Player = Struct.new(:name, :location)
+Room = Struct.new(:reference, :name, :description, :connections)
 
